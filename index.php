2f3789b737062be8bba1e9b19f0157949cbcc370
Christopher <salay777@hotmail.co.uk>
2017-04-05 13:31:17 +0000

    the first page is completed content wise

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <body>
             <div class="section animated fadeIn" id="riva">
                 <div class="row">
-                    <div class="col-lg-5">
+                    <div class="col-md-5 col-lg-5">
                         <h1 class="title animated fadeInRight" style="text-align:right">riva</h1>
                     </div>
                     <div class="col-lg-2">
@@ -15,13 +15,40 @@
                 </div>
             </div>
             <div class="section" id="hello">
-                <h1>section 2</h1>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1>hi , i am riva</h1>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <h2>I am a model</h2>
+                            <h2>music artist and a cast member of BkChat LDN</h2>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="section" id="follow_riva">
                 <h1>section 3</h1>
             </div>
             <div class="section" id="contact">
-                <h1>section 4</h1>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1>contact me</h1>
+                    </div>
+                </div>
+                
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <input type="text" name="name"> 
+                            </div>
+                            <div class="col-lg-6">
+                                <input type="email" name="email"/>
+                            </div>
+                        </div>
+                          <input type="textarea" name="message" rows="10" cols="30"> </br>
+                        
+                        <input type="submit" value="Submit"/>
+                </div>
             </div>
         </div>
     
