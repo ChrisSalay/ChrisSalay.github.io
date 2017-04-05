75ee1df54ba57f3c937e886bb54e30561bb8db1e
Christopher <salay777@hotmail.co.uk>
2017-04-05 14:11:20 +0000

    added the navigation bar

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.4/vendors/jquery.easings.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.4/vendors/scrolloverflow.min.js"></script>
 <script src="https://use.typekit.net/vis4ktf.js"></script>
+<script src="https://use.fontawesome.com/578402ce8e.js"></script>
 </html>
--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 <body>
             <div class="section animated fadeIn" id="riva">
                 <div class="row">
-                    <div class="col-md-5 col-lg-5">
+                    <div class="col-xs-5 col-sm-5 col-md-5 col-lg-5">
                         <h1 class="title animated fadeInRight" style="text-align:right">riva</h1>
                     </div>
-                    <div class="col-lg-2">
+                    <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
                         <h1 class="title animated fadeInDown" style="text-align:center">da</h1>
                     </div>
-                    <div class="col-lg-5">
+                    <div class="col-xs-5 col-sm-5 col-md-5 col-lg-5">
                         <h1 class="title animated fadeInLeft" style="text-align:left">diva</h1>
                     </div>
                     
@@ -16,11 +16,11 @@
             </div>
             <div class="section" id="hello">
                 <div class="row">
-                    <div class="col-lg-12">
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                         <h1>hi , i am riva</h1>
                     </div>
                     <div class="row">
-                        <div class="col-lg-12">
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                             <h2>I am a model</h2>
                             <h2>music artist and a cast member of BkChat LDN</h2>
                         </div>
@@ -32,16 +32,16 @@
             </div>
             <div class="section" id="contact">
                 <div class="row">
-                    <div class="col-lg-12">
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                         <h1>contact me</h1>
                     </div>
                 </div>
                 
                         <div class="row">
-                            <div class="col-lg-6">
+                            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
                                 <input type="text" name="name"> 
                             </div>
-                            <div class="col-lg-6">
+                            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
                                 <input type="email" name="email"/>
                             </div>
                         </div>
