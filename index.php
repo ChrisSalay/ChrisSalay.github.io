0ddeb8f01fe53824305115e6680654d3de91e959
Christopher <salay777@hotmail.co.uk>
2017-07-08 17:23:08 +0100

    fully responsive site

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
                         <i class="fa fa-twitter-square fa-5x" aria-hidden="true"></i>
                     </div>
                     <div class="col-lg-4">
-                        <i class="fa fa-facebook fa-5x" aria-hidden="true"></i>
+                        <i class="fa fa-facebook-square fa-5x" aria-hidden="true"></i>
                     </div>
                     <div class="col-lg-4">
                         <i class="fa fa-instagram fa-5x" aria-hidden="true"></i>
@@ -51,10 +51,10 @@
                 </div>
                 
                         <div class="row">
-                            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
                                 <input type="text" name="name"> 
                             </div>
-                            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
                                 <input type="email" name="email"/>
                             </div>
                         </div>
--- js/riva.js
+++ js/riva.js
@@ -1,1 +1,64 @@
-$(document).ready(function(){$("#fullpage").fullpage({navigation:!0,navigationTooltips:["Introduction","Hi I am Riva","Follow me","Contact me"],showActiveTooltip:!0,afterLoad:function(a,b){$(this);switch(b){case 1:$("#fp-nav ul li a span, .fp-slidesNav ul li a span").animate({backgroundColor:"white"}),$("nav").animate({backgroundColor:"black"}),$("#fp-nav ul li .fp-tooltip").animate({color:"#fff"});break;case 2:$("nav").animate({backgroundColor:"rgba(191,186,191,0.7)"}),$("#hello h1").slideDown(),$("#hello h2").fadeIn(),$("#fp-nav ul li .fp-tooltip").animate({color:"#fff"});break;case 3:$("#follow_riva i.fa.fa-twitter-square.fa-5x").fadeIn("slow",function(){$("#follow_riva i.fa.fa-facebook-square.fa-5x").fadeIn("slow",function(){$("#follow_riva i.fa.fa-instagram.fa-5x").fadeIn("slow")})}),$("#fp-nav ul li .fp-tooltip").animate({color:"#fff"});break;case 4:$("nav").animate({backgroundColor:"black"}),$("#fp-nav ul li .fp-tooltip").animate({color:"black"})}}});try{Typekit.load({async:!0})}catch(a){}$("i.fa.fa-bars.fa-5x").click(function(){$("#menu").fadeIn(500)}),$("i.fa.fa-times.fa-5x").click(function(){$("#menu").fadeOut(500)})});
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+	    navigation: true,
+	    navigationTooltips:['Introduction','Hi I am Riva','Follow me','Contact me'],
+	    showActiveTooltip: true,
+	    afterLoad: function(anchorLink, index){
+	    	var loadedSection = $(this);
+	    	
+	    		/*if(index == 1){
+		    		$("#fp-nav ul li a span, .fp-slidesNav ul li a span").animate({
+	    				backgroundColor : 'white'
+	    			}); */
+	    			
+	    			switch(index){
+	    				
+	    				case 1:
+	    				$("#fp-nav ul li a span, .fp-slidesNav ul li a span").animate({
+	    					backgroundColor : 'white'
+	    				});
+	    				$("nav").animate({backgroundColor: 'black'});
+	    				$("#fp-nav ul li .fp-tooltip").animate({color:'#fff'});
+	    				break;
+	    				
+	    				case 2:
+	    					$("nav").animate({backgroundColor: 'rgba(191,186,191,0.7)'});
+	    					$("#hello h1").slideDown();
+	    					$("#hello h2").fadeIn();
+	    					$("#fp-nav ul li .fp-tooltip").animate({color:'#fff'});
+	    				break;
+	    				
+	    				case 3:
+	    					$("#follow_riva i.fa.fa-twitter-square.fa-5x").fadeIn("slow",function(){
+	    						$("#follow_riva i.fa.fa-facebook-square.fa-5x").fadeIn("slow", function(){
+	    							$("#follow_riva i.fa.fa-instagram.fa-5x").fadeIn("slow");
+	    						});
+	    					});
+	    					$("#fp-nav ul li .fp-tooltip").animate({color:'#fff'});
+    					break;
+    					
+    					case 4:
+    						$("nav").animate({backgroundColor: 'black'});
+    						$("#fp-nav ul li .fp-tooltip").animate({color:'black'});
+    					break;
+	    				
+	    			}
+	    			
+	    			
+
+	    	} // end of after load
+	});
+	
+	try{
+		Typekit.load({ async: true });
+	}
+	catch(e){}
+	
+	$("i.fa.fa-bars.fa-5x").click(function(){
+			$("#menu").fadeIn(500);
+		}); // end of click event
+		
+	$("i.fa.fa-times.fa-5x").click(function(){
+			$("#menu").fadeOut(500);
+		}); // end of click event
+ }); // end of document.ready()
